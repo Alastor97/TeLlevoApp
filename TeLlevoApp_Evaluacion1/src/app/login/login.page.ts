fd1ce06e51d39d2e0adbb4f0ae43de589a3cd012
BastianCristofer <ba.recabarren@duocuc.cl>
2021-09-20 04:20:09 -0300

    login e iniciar sesion

--- TeLlevoApp_Evaluacion1/src/app/login/login.page.ts
+++ TeLlevoApp_Evaluacion1/src/app/login/login.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { TouchSequence } from 'selenium-webdriver';
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,8 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  // rescato desde el html el input #username
+  @ViewChild ('username') user;
+  // rescato desde el html el input #password
+  @ViewChild ('password') password;
 
-  constructor() { }
+  
+
+  constructor(private router: Router ) {}
+  login(){
+    if(this.user.value=="juancarlos00" && this.password.value== "123456" ){
+      this.router.navigate(['../perfil-user']);
+      
+    }
+  }
+ 
 
   ngOnInit() {
   }
